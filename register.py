d5b8260a35500b7844ec3c0dbf31f7dfb31e5c9d
srsato <sera.martin@gmail.com>
2021-12-09 20:30:02 +0100

    Registro

--- register.py
+++ register.py
@@ -3,10 +3,13 @@ from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField,
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Usuario', [validators.Length(min=4, max=25)])
-    email = StringField('E-mail', [validators.Length(min=6, max=35)])
+
+    username = StringField(
+        'Usuario', [validators.Length(min=4, max=25, message="Debe estar entre 4 y 25 caracteres")])
+    email = StringField(
+        'E-mail', [validators.Length(min=6, max=35, message="Debe estar entre 6 y 35 caracteres")])
     password = PasswordField('Clave', [
-        validators.DataRequired(),
+        validators.DataRequired(message="Campo obligatorio"),
         validators.EqualTo(
             'confirm', message='Las contraseñas deben coincidir')
     ])
