b010b5b93f41fe9005b1897169769c5a6356b2a4
srsato <sera.martin@gmail.com>
2021-12-15 20:06:49 +0100

    account funcional

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,35 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from flask_login import current_user
 from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField, validators, ValidationError
 from blog.modelos import Usuario
 
 
-class RegistrationForm(FlaskForm):
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Usuario', [validators.Length(min=4, max=25, message="Debe estar entre 4 y 25 caracteres")])
+    email = StringField(
+        'E-mail', [validators.Length(min=6, max=35, message="Debe estar entre 6 y 35 caracteres"), validators.Email(message="Escribe un mail válido.")])
+    picture = FileField('Cambiar imagen de perfil', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Actualizar')
 
+    def validate_usuario(self, username):
+        if username.data != current_user.username:
+            usuario = Usuario.query.filter_by(username=username.data).first()
+            if usuario:
+                raise ValidationError(
+                    "El nombre de usuario ya está en uso, por favor, escoge otro")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = Usuario.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError(
+                    "Ya existe una cuenta de usuario asocidada a ese email.")
+
+
+class RegistrationForm(FlaskForm):
     username = StringField(
         'Usuario', [validators.Length(min=4, max=25, message="Debe estar entre 4 y 25 caracteres")])
     email = StringField(
--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,45 @@
+import os
+import secrets
+from PIL import Image
 from flask import request, render_template, url_for, redirect, flash
 from blog import app, db, bcrypt, login_manager
 from blog.modelos import Usuario, Post
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
+    {
+        'autor': 'Pepe Gotera',
+        'titulo': 'Reforma Molona',
+        'fecha': '12-12-2012',
+        'contenido': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut architecto iste nulla voluptatibus recusandae, eligendi laborum consequuntur quis temporibus fuga delectus nisi error id necessitatibus maiores. Fugit, nostrum corporis.',
+        'color': 'fluorgreen',
+        'foto': 'https://www.reformasparaviviendas.com/wp-content/uploads/2020/04/Empresa-de-reformas-integrales-en-Valencia.jpg'
+    },
+    {
+        'autor': 'Rodolfo Langostino',
+        'titulo': 'Menu Nochebuena',
+        'fecha': '13-11-2011',
+        'contenido': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium illum officia iste, placeat ut, vitae maiores adipisci sapiente ex nisi nam delectus totam. Ducimus blanditiis impedit, maiores inventore magnam unde.',
+        'color': 'fluorpink',
+        'foto': "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fokdiario.com%2Fimg%2F2018%2F12%2F17%2Fmenu-recetas-de-navidad.jpg&f=1&nofb=1"
+    },
+    {
+        'autor': 'Pepe Gotera',
+        'titulo': 'Reforma Molona',
+        'fecha': '12-12-2012',
+        'contenido': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut architecto iste nulla voluptatibus recusandae, eligendi laborum consequuntur quis temporibus fuga delectus nisi error id necessitatibus maiores. Fugit, nostrum corporis.',
+        'color': 'fluorgreen',
+        'foto': 'https://www.reformasparaviviendas.com/wp-content/uploads/2020/04/Empresa-de-reformas-integrales-en-Valencia.jpg'
+    },
+    {
+        'autor': 'Rodolfo Langostino',
+        'titulo': 'Menu Nochebuena',
+        'fecha': '13-11-2011',
+        'contenido': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium illum officia iste, placeat ut, vitae maiores adipisci sapiente ex nisi nam delectus totam. Ducimus blanditiis impedit, maiores inventore magnam unde.',
+        'color': 'fluorpink',
+        'foto': "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fokdiario.com%2Fimg%2F2018%2F12%2F17%2Fmenu-recetas-de-navidad.jpg&f=1&nofb=1"
+    },
     {
         'autor': 'Pepe Gotera',
         'titulo': 'Reforma Molona',
@@ -73,6 +108,52 @@ def registro():
         flash(f'Cuenta de {form.username.data} creada con exito', 'success')
         return redirect(url_for('login'))
     return render_template('registro.html', title="Registro", form=form)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    print(picture_fn)
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+
+    if form.validate_on_submit():
+        print(form.username.data)
+        if form.picture.data:
+            if current_user.image_file and current_user.image_file != 'default.jpg':
+                a_borrar = os.path.join(
+                    app.root_path, 'static/profile_pics', current_user.image_file)
+                try:
+                    os.remove(a_borrar)
+                except FileNotFoundError:
+                    pass
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        print(current_user.image_file)
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        print("Comiteando")
+        db.session.commit()
+        flash('Cuenta actualizada, mucho más molona así...', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Mi cuenta', image_file=image_file, form=form)
 
 
 @app.route("/gaslands")
